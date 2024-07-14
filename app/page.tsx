d3b5ee3709a0b4ca39d59ba24d599fb246ad90c1
Nguyen Minh Khanh <khanhnm222@gmail.com>
2024-07-15 00:42:53 +0700

    feat: Create Banner component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Banner from "@/components/features/banner/banner";
 import Category from "@/components/features/category/category";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Banner />
       <Category />
     </main>
   );
