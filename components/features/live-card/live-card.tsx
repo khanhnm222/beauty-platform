5d60e386862af495711308ae0025a9132142a16b
Nguyen Minh Khanh <khanhnm222@gmail.com>
2024-07-18 09:23:20 +0700

    feat: Separate Live card component

---
+++ components/features/live-card/live-card.tsx
@@ -0,0 +1,23 @@
+interface LiveCardProps {
+  avatar: string;
+  name: string;
+  memberCount?: string
+  onlineCount?: string;
+};
+
+const LiveCard = ({
+  avatar,
+  name,
+  memberCount,
+  onlineCount,
+}: LiveCardProps) => {
+  return (
+    <div className="relative group bg-primary-2 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-primary-2/80 hover:smooth-hover">
+      <img className="w-20 h-20 object-cover object-center rounded-full" src={avatar} alt="cuisine" />
+      <h4 className="text-white text-2xl font-bold capitalize text-center">{name}</h4>
+      <p className="text-white/80">{memberCount} members</p>
+      <p className="absolute top-2 text-black inline-flex items-center text-xs">{onlineCount} Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
+    </div>
+  );
+};
+export default LiveCard;
