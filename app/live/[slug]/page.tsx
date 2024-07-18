c94747c67af159e2ef50c9b5f36017fc00c3dd4e
Nguyen Minh Khanh <khanhnm222@gmail.com>
2024-07-18 21:43:59 +0700

    feat: Create Live detail page

---
+++ app/live/[slug]/page.tsx
@@ -0,0 +1,8 @@
+const LiveDetail = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>This is a live detail</div>
+    </main>
+  );
+};
+export default LiveDetail;
--- components/features/live-card/live-card.tsx
+++ components/features/live-card/live-card.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+
 interface LiveCardProps {
   avatar: string;
   name: string;
@@ -11,8 +15,12 @@ const LiveCard = ({
   memberCount,
   onlineCount,
 }: LiveCardProps) => {
+  const router = useRouter()
   return (
-    <div className="relative group bg-primary-2 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-primary-2/80 hover:smooth-hover">
+    <div
+      className="relative group bg-primary-2 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-primary-2/80 hover:smooth-hover"
+      onClick={() => router.push('/live/name', { scroll: false })}
+    >
       <img className="w-20 h-20 object-cover object-center rounded-full" src={avatar} alt="cuisine" />
       <h4 className="text-white text-2xl font-bold capitalize text-center">{name}</h4>
       <p className="text-white/80">{memberCount} members</p>
