95dec9fd3391197fa861ca193b7a6164e46280de
Nguyen Minh Khanh <khanhnm222@gmail.com>
2024-07-24 00:50:24 +0700

    feat: Temporary adding Profile and Settings pages

---
+++ app/profile/page.tsx
@@ -0,0 +1,8 @@
+const Profile = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>This is a profile page</div>
+    </main>
+  );
+};
+export default Profile;
---
+++ app/settings/page.tsx
@@ -0,0 +1,8 @@
+const Settings = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>This is a setting page</div>
+    </main>
+  );
+};
+export default Settings;
