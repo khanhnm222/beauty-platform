70cd6dbc5b7464afb8509358ce6956f8612f3717
Nguyen Minh Khanh <khanhnm222@gmail.com>
2024-07-28 19:39:37 +0700

    fix: correct main contents

--- components/features/banner/banner.tsx
+++ components/features/banner/banner.tsx
@@ -10,11 +10,11 @@ const Banner = () => {
       <div className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
 
       <div className={cn(
-        "relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8 flex items-end",
+        "relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:items-center lg:px-8 flex items-end sm:pt-[15rem]", // lg:h-screen
         style.textContainer
       )}>
         <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-          <h1 className="text-3xl font-extrabold text-primary/80 sm:text-5xl">
+          <h1 className="text-3xl font-extrabold text-primary/80 sm:text-4xl">
             Let us find your
             <strong className="block font-extrabold text-primary">
               {" "}
